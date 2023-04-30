6f5c08c937b9ec93f2a25808c2bcd437ede4cd82
hiro08gh <hiro08gh@gmail.com>
2023-04-30 14:02:56 +0900

    add: initial folders

---
+++ libs/meta.ts
@@ -0,0 +1,5 @@
+export const meta = {
+  title: "simple-job-board-template",
+  description: "description",
+  site_url: "",
+} as const;
---
+++ libs/microcms.ts
@@ -0,0 +1,52 @@
+import { createClient } from "microcms-js-sdk";
+import type { MicroCMSQueries, MicroCMSDate } from "microcms-js-sdk";
+
+export interface JobType extends MicroCMSDate {
+  id: string;
+  title?: string;
+  description?: string;
+  content?: string;
+}
+
+export interface CategoryType extends MicroCMSDate {
+  id: string;
+  name?: string;
+}
+
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || "",
+  apiKey: process.env.MICROCMS_API_KEY || "",
+});
+
+// 求人一覧を取得
+export const getJobList = async (queries?: MicroCMSQueries) => {
+  return client.getList<JobType>({
+    endpoint: "jobs",
+    queries,
+  });
+};
+
+// 求人の内容を取得
+export const getJobDetail = async (id: string, draftKey?: string) => {
+  return client.getListDetail<JobType>({
+    endpoint: "jobs",
+    contentId: id,
+    queries: { draftKey: draftKey ?? "" },
+  });
+};
+
+// カテゴリー覧を取得
+export const getCategoryList = async (queries?: MicroCMSQueries) => {
+  return client.getList<CategoryType>({
+    endpoint: "category",
+    queries,
+  });
+};
+
+// カテゴリーの内容を取得
+export const getCategoryDetail = async (id: string) => {
+  return client.getListDetail<CategoryType>({
+    endpoint: "category",
+    contentId: id,
+  });
+};
