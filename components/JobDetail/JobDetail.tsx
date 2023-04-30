5794b69b2378463d7a92dcf373f7312b0f8d24bd
hiro08gh <hiro08gh@gmail.com>
2023-04-30 17:02:10 +0900

    chore: adjust style

--- components/JobDetail/JobDetail.tsx
+++ components/JobDetail/JobDetail.tsx
@@ -10,16 +10,18 @@ type Props = {
 export const JobDetail: React.FC<Props> = ({ job }) => {
   return (
     <div className={styles.job}>
-      <h1 className={styles.title}>{job.title}</h1>
-      <Link href={`/categories/${job.category?.id}`}>
-        <span className={styles.categoryName}>#{job?.category?.name}</span>
-      </Link>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `${job.content}`,
-        }}
-        className={styles.content}
-      />
+      <div className={styles.inner}>
+        <h1 className={styles.title}>{job.title}</h1>
+        <Link href={`/categories/${job.category?.id}`}>
+          <span className={styles.categoryName}>#{job?.category?.name}</span>
+        </Link>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `${job.content}`,
+          }}
+          className={styles.content}
+        />
+      </div>
     </div>
   );
 };
