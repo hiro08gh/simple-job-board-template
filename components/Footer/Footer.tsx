350a132c49911113a2fc94e18872c7a01c9d35bd
hiro08gh <hiro08gh@gmail.com>
2023-05-01 09:03:46 +0900

    chore: change footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
+import { meta } from "@/libs/meta";
 import styles from "./footer.module.scss";
 
 export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.wrapper}>
-        <footer className={styles.title}>Copyright ©︎ simple job site.</footer>
+        <footer className={styles.title}>Copyright ©︎ {meta.title}.</footer>
       </div>
     </footer>
   );
